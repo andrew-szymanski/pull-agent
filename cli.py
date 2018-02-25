44c9192fe73ed8fce0b6d7db3cf3a22846678481
andrew-szymanski <madpole@donotreply.com>
2018-02-25 12:25:42 +0100

    ongoing

--- cli.py
+++ cli.py
@@ -22,10 +22,57 @@ logger.addHandler(console)
 logger.setLevel(logging.DEBUG)     # default, this will be reset later
 
 
+
+class Poller(object):
+   """ Class for polling functionality
+   """
+   def __init__(self, **kwargs):
+      """Create an object and attach or initialize logger
+      """
+      self.logger = kwargs.get('logger',None)
+      if ( self.logger is None ):
+            # Get an instance of a logger
+            self.logger = logger
+      # initial log entry
+      self.logger.setLevel(logger.getEffectiveLevel())
+      self.logger.debug("%s: %s version [%s]" % (self.__class__.__name__, inspect.getfile(inspect.currentframe()),__version__))
+      
+
+   def run(self, *args, **kwargs):
+      """ run singleton logger
+      """
+      self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3])) 
+      self.logger.debug("args: [%s]" % args)
+      
+
+
+
+
+
+
 def run_poller_command(command, args):
     logger.info("%s starting..." % inspect.stack()[0][3])
     logger.debug("command: [%s], args: [%s]" % (command, args))
+
+    # switch
+    if ( command == 'run' ):
+        logger.debug("creating poller object...") 
+        poller = Poller(logger=logger)
+        poller.run(args)
+        #logger.debug("setting up Helper_Manager...") 
+
+    # try:
+    #     # execute specified class method
+    #     poller.run(logger=logger, args)   
+    # except Exception as caughtException:
+    #     print "ERROR: [%s]" % (caughtException,'\n')
+    #     sys.exit(1)
+    
+
+    logger.info("all done")          
  
+
+
 def run_main(opts, args):
     logger.info("%s starting..." % inspect.stack()[0][3])
 
