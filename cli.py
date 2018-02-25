1a6233f1f8ce4dc453e1c6c1e9956dac008f8b7d
andrew-szymanski <madpole@donotreply.com>
2018-02-25 13:09:34 +0100

    ongoing

--- cli.py
+++ cli.py
@@ -25,6 +25,7 @@ logger.addHandler(console)
 logger.setLevel(logging.DEBUG)     # default, this will be reset later
 
 LOCK_FILE_FULLPATH="/tmp/polling-deployer.lck"
+STOP_FILE_FULLPATH="/tmp/polling-deployer.stop"
 
 
 
@@ -48,7 +49,7 @@ class Poller(object):
         """
         self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3])) 
         self.logger.debug("args: [%s]" % args)
-        self.logger.debug("lockflie: [%s]" % LOCK_FILE_FULLPATH)
+        self.logger.debug("lockfile: [%s]" % LOCK_FILE_FULLPATH)
         #
         lock_file = LOCK_FILE_FULLPATH
         fp = open(lock_file, 'w')
@@ -60,9 +61,26 @@ class Poller(object):
             sys.exit(0)
       
         self.logger.info("poller starting...")
+        # check if stop file exists and make sure it doesn't
+        if os.path.exists(STOP_FILE_FULLPATH):
+            self.logger.info("stop file exists on start - removing it")
+            os.remove(STOP_FILE_FULLPATH)
         self.poll()
 
-
+    def stop(self, *args, **kwargs):
+        """ stop singleton logger
+        """
+        self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3])) 
+        self.logger.debug("stopfile: [%s]" % STOP_FILE_FULLPATH)
+        #
+        stop_file = STOP_FILE_FULLPATH
+        fp = open(stop_file, 'w')
+        try:
+            fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except IOError:
+            # another instance is running
+            self.logger.warn("stop command already issued")
+            sys.exit(0)
 
     def poll(self):
         """ main polling loop
@@ -71,6 +89,12 @@ class Poller(object):
         while True:
             time.sleep(2)
             self.logger.debug("going to sleep...")
+
+            # check if stop file exists
+            if os.path.exists(STOP_FILE_FULLPATH):
+                self.logger.info("request to stop received...")
+                os.remove(STOP_FILE_FULLPATH)
+                exit(0)
 
 
 
@@ -83,7 +107,13 @@ def run_poller_command(command, args):
         logger.debug("starting polller...")
         poller = Poller(logger=logger)
         poller.run(args)
-        #logger.debug("setting up Helper_Manager...") 
+        return;
+
+    if ( command == 'stop' ):
+        logger.debug("stopping polller...")
+        poller = Poller(logger=logger)
+        poller.stop(args)
+        return;        
 
     # try:
     #     # execute specified class method
